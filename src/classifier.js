86124a7e7a1d6b387d9239824b64a5967df6a0c7
josepmsan <18270604+josepmsan@users.noreply.github.com>
2025-06-04 01:26:47 +0200

    +test v1 for classifying ticket

--- src/classifier.js
+++ src/classifier.js
@@ -2,6 +2,10 @@ class Classifier {
     say_hello() {
         return "say hello";
     }
+
+    classify(text) {
+        return ""
+    }
 }
 
 module.exports = Classifier;
--- src/index.test.js
+++ src/index.test.js
@@ -3,3 +3,7 @@ const Classifier = require('./classifier');
 test('Classifier says hello', () => {
   expect(new Classifier().say_hello()).toBe("say hello");
 });
+
+test('Classifier processes ticket 1', () => {
+  expect(new Classifier().classify()).toBe("dummy");
+});
