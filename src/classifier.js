283e2a2a389ebf55e851eb0a82765603c224bab8
josepmsan <18270604+josepmsan@users.noreply.github.com>
2025-06-04 02:16:13 +0200

    testing prompts using jest tests

--- src/classifier.js
+++ src/classifier.js
@@ -6,15 +6,32 @@ export default class Classifier {
     }
 
     async classify(text) {
-        return await send_msg_to_ollama();
+        return await send_msg_to_ollama(text);
     }
 }
 
-async function send_msg_to_ollama() {
+async function send_msg_to_ollama(ticket_text) {
+    const prompt = `Classify the following support ticket into:
+        - Intent (e.g., refund, question, billing, shipping, order, claim)
+        - Urgency (low, medium, high)
+        - Product (Online Shop, Product Reviews, Chat Support)
+
+        Ticket: "${ticket_text}"
+
+        Respond in a javascript JSON format following the format like:
+        {
+            "intent": "...",
+            "urgency": "...",
+            "product": "...",
+            "ticket_text": "${ticket_text}"
+        }
+
+        I don't want any other info than the one in JSON format with correct syntax.
+    `
     const response = await ollama.chat({
         model: 'llama3',
         messages: [
-        { role: 'user', content: 'What is the capital from Catalonia?' }
+        { role: 'user', content: prompt }
         ],
     });
 
@@ -22,5 +39,3 @@ async function send_msg_to_ollama() {
     console.log(content);
     return content;
 }
-
-// module.exports = Classifier;
--- src/index.test.js
+++ src/index.test.js
@@ -5,5 +5,17 @@ test('Classifier says hello', () => {
 });
 
 test('Classifier processes ticket 1', async () => {
-  expect(await new Classifier().classify()).toBe("dummy");
-});
+  expect(await new Classifier().classify("When would it be my next payment?")).toBe("dummy");
+}, 100000);
+
+test('Classifier processes ticket 2', async () => {
+  expect(await new Classifier().classify("I wasn't happy with the attention received by Norbert, I demand compensation!")).toBe("dummy");
+}, 100000);
+
+test('Classifier processes ticket 3', async () => {
+  expect(await new Classifier().classify("When will there be again dotted socks?")).toBe("dummy");
+}, 100000);
+
+test('Classifier processes ticket 4', async () => {
+  expect(await new Classifier().classify("I can't see where to generate all my prvious invoices, my accountant is urgently asking me for them!")).toBe("dummy");
+}, 100000);
