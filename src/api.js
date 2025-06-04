42af7a0b272b98803d1680770db263c9cde68a5c
josepmsan <18270604+josepmsan@users.noreply.github.com>
2025-06-04 03:29:09 +0200

    +versioning to classify_with_ollama_using_llama_3

--- src/api.js
+++ src/api.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.send('Miau!');
 });
 
-app.post('/classify_with_ollama_using_llama_3', (req, res) => {
+app.post('/v1/classify_with_ollama_using_llama_3', (req, res) => {
     const {tickets = []} = req.body;
     const response = new OllamaClassifierController().classify_tickets(tickets);
     res.send('Miau!');
